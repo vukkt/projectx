4c347c630b9c4f51bfdc1adb65647bb54e992423
Vuk Topalovic <63306506+vukkt@users.noreply.github.com>
2023-09-21 17:53:51 +0200

    Keep local .gitignore changes

---
+++ src/test/java/com/projectx/ProjectxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
